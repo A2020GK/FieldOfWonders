360243960f8ae23ce4ea3d46444c54d7c2452f96
Antony <a2020gk@outlook.com>
2025-04-19 00:38:28 +0300

    Add comments

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
-import random
-from colorama import Fore,Style,Back
-import readchar
-import time
+import random # Module for randomness
+from colorama import Fore,Style,Back # Module for color in terminal
+import readchar # Module for reading character from terminal, skipping <Enter> key in default input()
+import time # Module for.. sleep
 
-clear = lambda: print("\033c\033[3J", end='') 
+clear = lambda: print("\033c\033[3J", end='') # Function to clear the terminal (any OS) 
 clear()
 
 print(f"""{Back.CYAN}                
    ПОЛЕ ЧУДЕС   
                 {Style.RESET_ALL}""".strip())
 print("Загрузка слов...")
-words = open("russian_nouns.txt", "r", encoding="utf-8").read().split("\n")
+
+words = open("russian_nouns.txt", "r", encoding="utf-8").read().split("\n") # Read words from file
+
 print(f"Загрузка завершена. Загруженно {len(words)} слов.")
 print("Старт...")
 
-word = random.choice(words)
-guessed = set()
-op = 0
+word = random.choice(words) # Select random word
+guessed = set() # Set for guessed letters
+op = 0 # Operation variable for command system
 
 time.sleep(0.5)
 
@@ -28,15 +30,19 @@ if not attempts.isdigit():
 attempts = int(attempts)
 
 def render():
+    # Render pseudo-graphics
     tbsplitter = ["━━━"] * len(word)
     result = "┏" + "┳".join(tbsplitter) + "┓\n┃ "
     for i in word:
         result += (f"{Fore.GREEN if op == 1 else ""}{i}{Fore.RESET}" if i in guessed else " ") + " ┃ "
     result = result.strip()
     result += "\n┗" + "┻".join(tbsplitter) + "┛"
+    
+    # Print everything
     clear()
     print(result, end="\n\n")
     
+    # Check status operation variable
     if op == 1:
         print(f"{Back.GREEN}Вы победили!{Back.RESET}\n")
     elif op == 2:
@@ -49,23 +55,23 @@ def render():
     print(f"Угаданно букв: {Fore.CYAN}{len(guessed)}{Fore.RESET}\n")
     print("Команды:\n0\tВыход\n1\tПравила\n")
 
-char = ""
+char = "" # Buffer for input character
 render()
 
 try:
     while True:
         char = readchar.readchar().lower()
-        if char == "0":
+        if char == "0": # Exit
             op = 2
             break
-        elif char == "1":
+        elif char == "1": # Help
             op = 3
-        elif char in word:
-            if char in guessed:
+        elif char in word: # If word exists
+            if char in guessed: # If word already guessed
                 op = 4
             else:
                 op = 0
-                guessed.add(char)
+                guessed.add(char) # Add letter
                 if set(word) == guessed:
                     op = 1
                     break
@@ -78,5 +84,5 @@ try:
         render()
     render()
     print(f"Слово: {word}")
-except KeyboardInterrupt:
-    print(Style.RESET_ALL)
+except KeyboardInterrupt: 
+    print(Style.RESET_ALL) # Reset style to remove colors from terminal after forceful exit
